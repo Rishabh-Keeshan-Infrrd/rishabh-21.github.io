f2367be7f69995c909a1625eafba1541e2df1384
rishabh-21 <rishabhkeeshan@infrrd.ai>
2020-11-12 22:59:06 +0530

    fixed signup component for design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {SignupComponent} from './auth-component/signup/signup.component';
+import {AuthComponentComponent} from './auth-component/auth-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignupComponent,
+    AuthComponentComponent
   ],
   imports: [
     BrowserModule,
