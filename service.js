28629e4e0047f5d09067dc69dfb5252562efaa48
rishabh-21 <rishabhkeeshan@infrrd.ai>
2020-11-24 18:57:32 +0530

    heroku config updated postinstall

--- service.js
+++ service.js
@@ -16,14 +16,14 @@ const forceSSL = function() {
 };
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/<name-of-app>'));
+app.use(express.static(__dirname + '/dist/tyro'));
 
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/tyro/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 4200);
 
 app.use(forceSSL());
 
