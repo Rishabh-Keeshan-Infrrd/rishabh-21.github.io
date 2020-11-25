3e5947e6f6a3cfba0f5765d276fcc155585c56ea
rishabh-21 <rishabhkeeshan@infrrd.ai>
2020-11-25 22:52:37 +0530

    heroku config service port

--- service.js
+++ service.js
@@ -28,7 +28,7 @@ app.get('/assets', function(req, res) {
 
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 35474);
+app.listen(process.env.PORT || 8080);
 
 app.use(forceSSL());
 
